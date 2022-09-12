6812664d19702d46691af77a93c71fcc44700a6c
Aliya Abdullah <aliya_abd2000@yahoo.in>
2022-09-11 21:17:02 -0400

    all base test cases passed

--- edu/ufl/cise/plpfa22/Lexer.java
+++ edu/ufl/cise/plpfa22/Lexer.java
@@ -1,47 +1,71 @@
 package edu.ufl.cise.plpfa22;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.ufl.cise.plpfa22.IToken.Kind;
 
 public class Lexer implements ILexer {
 	
 	public static enum State{
-		START, 
-		
-		HAVE_COLON,
-		
-		//accepting states
+		START, 	
 		HAVE_LSTHAN, //less than
 		HAVE_GRTHAN,
-		HAVE_ASSGN, //:=
-		HAVE_IDENT
+		HAVE_IDENT,
+		HAVE_NUM,
+		HAVE_STRING,
+		HAVE_WHITESPACE
 		
 	}
+	
+	public int current_token = 0;
+	public ArrayList<Token> tokenset = new ArrayList<Token>();
+	static final char EOF = 0;
 
 	public Lexer(String input) {
 		
+		// adding eof to input
 		char[] chinp = input.toCharArray();
-		char EOF = 0;
+		
 		int len = input.length();
 		char[] inp_eof= new char[len+1];
-		for (int i=0; i<len-1; i++)
+		for (int i=0; i<len; i++)
 		{
 			inp_eof[i]=chinp[i];
 		}
-		inp_eof[len-1]=EOF;
+		inp_eof[len]=EOF;
 		
+		//initializing variables
 		int lexer_pos=0;
 		int token_pos=0;
-		int line=0;
+		int line=1;
 		int gpos=0;
 		State state = State.START;
+	
+		boolean eof_enc = true;
+		char[] dummy = new char[1];
 		
-		ArrayList<Token> tokenset = new ArrayList<Token>();
+
 		
-		while (true)
+		//creating map for reserved words
+		Map<String, Kind> reserved = new HashMap<String, Kind>();
+		reserved.put("TRUE",Kind.BOOLEAN_LIT);
+		reserved.put("FALSE",Kind.BOOLEAN_LIT);
+		reserved.put("CONST",Kind.KW_CONST);
+		reserved.put("VAR",Kind.KW_VAR);
+		reserved.put("PROCEDURE",Kind.KW_PROCEDURE);
+		reserved.put("CALL",Kind.KW_CALL);
+		reserved.put("BEGIN",Kind.KW_BEGIN);
+		reserved.put("END",Kind.KW_END);
+		reserved.put("IF",Kind.KW_IF);
+		reserved.put("THEN",Kind.KW_THEN);
+		reserved.put("WHILE",Kind.KW_WHILE);
+		reserved.put("DO",Kind.KW_DO);
+		
+		while (eof_enc)
 		{
-			char ch = inp_eof[lexer_pos];
+			char ch = inp_eof[gpos];
 			switch(state)
 			{
 			case START ->
@@ -52,7 +76,7 @@ public class Lexer implements ILexer {
 				// single character tokens
 				case '.' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.DOT, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.DOT, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -60,7 +84,7 @@ public class Lexer implements ILexer {
 				
 				case ',' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.COMMA, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.COMMA, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -68,7 +92,7 @@ public class Lexer implements ILexer {
 				
 				case ';' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.SEMI, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.SEMI, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -76,7 +100,7 @@ public class Lexer implements ILexer {
 				
 				case '(' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.LPAREN, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.LPAREN, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -84,7 +108,7 @@ public class Lexer implements ILexer {
 				
 				case ')' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.RPAREN, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.RPAREN, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -92,7 +116,7 @@ public class Lexer implements ILexer {
 				
 				case '+' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.PLUS, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.PLUS, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -100,7 +124,7 @@ public class Lexer implements ILexer {
 				
 				case '-' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.MINUS, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.MINUS, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -108,7 +132,7 @@ public class Lexer implements ILexer {
 				
 				case '*' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.TIMES, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.TIMES, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -116,7 +140,7 @@ public class Lexer implements ILexer {
 				
 				case '%' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.MOD, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.MOD, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -124,7 +148,7 @@ public class Lexer implements ILexer {
 				
 				case '?' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.QUESTION, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.QUESTION, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -132,7 +156,7 @@ public class Lexer implements ILexer {
 				
 				case '!' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.BANG, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.BANG, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -140,7 +164,7 @@ public class Lexer implements ILexer {
 				
 				case '=' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.EQ, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.EQ, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -148,65 +172,96 @@ public class Lexer implements ILexer {
 				
 				case '#' ->
 				{
-					Token t = new Token(String.valueOf(ch), Kind.NEQ, token_pos, line, lexer_pos, 1);
+					Token t = new Token(String.valueOf(ch), Kind.NEQ, token_pos, line, lexer_pos, 1, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
 				}
 				
+				case EOF ->
+				{
+					Token t = new Token("EOF", Kind.EOF, token_pos, line, gpos, 1, dummy);
+					tokenset.add(t);
+					eof_enc=false;
+				}
+				
+				case ' ' ->
+				{
+					lexer_pos++;
+					gpos++;
+				}
+				
+				case '\n' ->
+				{
+					lexer_pos = 0;
+					gpos++;
+					line++;
+
+				}
+				
+				case '\r' ->
+				{
+					lexer_pos++;
+					gpos++;
+				}
+				
+				case '\t' ->
+				{
+					lexer_pos++;
+					gpos++;
+				}
 				//Division or comment
 				
 				case '/' ->
 				{
-					char next = inp_eof[token_pos+1];
+					char next = inp_eof[gpos+1];
 					
 					//comment present
 					if (next=='/')
 					{
 						gpos=gpos+2;
-						next = inp_eof[gpos];
-						char next2 = inp_eof[gpos+1];
+						char n = inp_eof[gpos];
 						
 						//while next characters aren't \n, \r or eof
-						while((next!='\\' && (next2!='n' || next2!='r')) || next!=EOF)
+						while(n!=(char)10 && n!='\r' && n!=EOF)
 						{
 							gpos++;
-							next = inp_eof[gpos];
-							next2 = inp_eof[gpos+1];
+							n = inp_eof[gpos];
 							
 						}
 						
 						//eof encountered
-						if (next==EOF)
+						if (n==EOF)
 						{
-							Token t = new Token("EOF", Kind.EOF, token_pos, line, gpos, 1);
+							Token t = new Token("EOF", Kind.EOF, token_pos, line, gpos, 1, dummy);
 							tokenset.add(t);
 							
 						}
 						
 						//line ended; reset position to 0 and go to next line
-						else
+						
+						
+						if(n=='\r')
 						{
-							if(next2=='r')
-							{
 								line++;
-								gpos=gpos+3;
+								gpos=gpos++;
 								lexer_pos=0;
-							}
-							if(next2=='n')
-							{
+						}
+						if(n=='\n')
+						{
 								line++;
 								gpos++;
 								lexer_pos=0;
-							}
+								System.out.println("in line end");
 						}
+						
 					}
 					//comments handled
 					
 					//just division symbol
 					else
 					{
-						Token t = new Token(String.valueOf(ch), Kind.DIV, token_pos, line, gpos, 1);
+						Token t = new Token(String.valueOf(ch), Kind.DIV, token_pos, line, gpos, 1, dummy);
 						tokenset.add(t);
 						lexer_pos++;
 						gpos++;
@@ -218,14 +273,14 @@ public class Lexer implements ILexer {
 				{
 					if(inp_eof[gpos+1] == '=')
 					{
-						Token t = new Token(":=", Kind.ASSIGN, token_pos, line, lexer_pos, 2);
+						Token t = new Token(":=", Kind.ASSIGN, token_pos, line, lexer_pos, 2, dummy);
 						tokenset.add(t);
 						lexer_pos = lexer_pos+2;
 						gpos = gpos +2;
 					}
 					else
 					{
-						Token t = new Token(":=", Kind.ERROR, token_pos, line, lexer_pos, 1);
+						Token t = new Token(":=", Kind.ERROR, token_pos, line, lexer_pos, 1, dummy);
 						tokenset.add(t);
 						lexer_pos++;
 						gpos++;
@@ -255,7 +310,54 @@ public class Lexer implements ILexer {
 				'R','S','T','U','V','W','X','Y','Z',
 				'_','$' ->
 				{
+					token_pos=lexer_pos;
+					lexer_pos++;
+					gpos++;
 					state = State.HAVE_IDENT;
+				}
+				
+				//0
+				case '0'->
+				{
+					Token t = new Token("0", Kind.NUM_LIT, token_pos, line, gpos, 1, dummy);
+					tokenset.add(t);
+					lexer_pos++;
+					gpos++;
+				}
+				
+				//numbers
+				case '1','2','3','4','5','6','7','8','9' ->
+				{
+					token_pos=lexer_pos;
+					lexer_pos++;
+					gpos++;
+					state = State.HAVE_NUM;
+				}
+				
+				//string literal
+				case '"' ->
+				{
+					token_pos=lexer_pos;
+
+					lexer_pos++;
+					gpos++;
+					state = State.HAVE_STRING;
+				}
+				
+				//whitespace
+				case '\\' ->
+				{
+					lexer_pos++;
+					gpos++;
+					state = State.HAVE_WHITESPACE;
+				}
+				
+				default ->
+				{
+					Token t = new Token("Character is Invalid!", Kind.ERROR, token_pos, line, gpos, 1, dummy);
+					tokenset.add(t);
+					lexer_pos++;
+					gpos++;
 				}
 				}
 			}
@@ -269,7 +371,7 @@ public class Lexer implements ILexer {
 				{
 				case '=' ->
 				{
-					Token t = new Token("<=", Kind.LE, lexer_pos-1, line, lexer_pos-1, 2);
+					Token t = new Token("<=", Kind.LE, lexer_pos-1, line, lexer_pos-1, 2, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -277,7 +379,7 @@ public class Lexer implements ILexer {
 				}
 				default ->
 				{
-					Token t = new Token("<", Kind.LT, lexer_pos-1, line, lexer_pos-1, 1);
+					Token t = new Token("<", Kind.LT, lexer_pos-1, line, lexer_pos-1, 1, dummy);
 					tokenset.add(t);
 					state = State.START;
 				}
@@ -292,7 +394,7 @@ public class Lexer implements ILexer {
 				{
 				case '=' ->
 				{
-					Token t = new Token(">=", Kind.GE, lexer_pos-1, line, lexer_pos-1, 2);
+					Token t = new Token(">=", Kind.GE, lexer_pos-1, line, lexer_pos-1, 2, dummy);
 					tokenset.add(t);
 					lexer_pos++;
 					gpos++;
@@ -300,7 +402,7 @@ public class Lexer implements ILexer {
 				}
 				default ->
 				{
-					Token t = new Token(">", Kind.GT, lexer_pos-1, line, lexer_pos-1, 1);
+					Token t = new Token(">", Kind.GT, lexer_pos-1, line, lexer_pos-1, 1, dummy);
 					tokenset.add(t);
 					state = State.START;
 				}
@@ -312,9 +414,8 @@ public class Lexer implements ILexer {
 			//
 			case HAVE_IDENT ->
 			{
-				token_pos = lexer_pos;
-				lexer_pos++;
-				gpos++;
+
+				ch=inp_eof[gpos];
 				
 				switch(ch)
 				{
@@ -333,48 +434,176 @@ public class Lexer implements ILexer {
 				//parsed identifier
 				default ->
 				{
-					int ident_len = lexer_pos - token_pos + 1;
+					int ident_len = lexer_pos - token_pos;
 					char[] ident = new char[ident_len];
 					for(int i=0;i<ident_len;i++)
 					{
-						ident[i]=inp_eof[gpos-lexer_pos+i];
+						ident[i]=inp_eof[gpos-ident_len+i];
 					}
+					
+					//ident stored in identifier
 					String identifier = new String(ident);
-					Token t = new Token(identifier, Kind.STRING_LIT, token_pos, line, gpos, ident_len);
-					tokenset.add(t);
-					gpos++;
-					lexer_pos++;
-					state = State.START;
 					
+					//is it reserved?
+					if(reserved.containsKey(identifier))
+					{
+						Token t = new Token(identifier, reserved.get(identifier), token_pos, line, gpos-ident_len, ident_len, dummy);
+						tokenset.add(t);	
+					}
 					
+					//it is identifier
+					else 
+					{
+						//System.out.println(identifier);
+						//System.out.println(gpos);
+						Token t = new Token(identifier, Kind.IDENT, token_pos, line, gpos-ident_len, ident_len, dummy);
+						tokenset.add(t);
+					}
+
+					state = State.START;							
 				}
 				}
 			}
+			//identifier case over
+			
+			//number 
+			case HAVE_NUM ->
+			{
+				
+				ch = inp_eof[gpos];
+				
+				switch(ch)
+				{
+				
+				case '0','1','2','3','4','5','6','7','8','9' ->
+				{
+					lexer_pos++;
+					gpos++;
+				}
+				
+				default ->
+				{
+					int num_len = lexer_pos - token_pos;
+					char[] num = new char[num_len];
+					for(int i=0;i<num_len;i++)
+					{
+						num[i]=inp_eof[gpos-num_len+i];
+					}
+					
+					//number converted to string
+					String snum = new String(num);
+					System.out.println(snum);
+					
+					//catch number too big exceptions
+					try 
+					{
+						int number = Integer.parseInt(snum);
+						Token t = new Token(snum, Kind.NUM_LIT, token_pos, line, gpos-num_len, num_len, dummy);
+						tokenset.add(t);
+					}
+					catch (NumberFormatException e)
+					{
+						Token t = new Token("Number too big!", Kind.ERROR, token_pos, line, gpos-num_len, num_len, dummy);
+						tokenset.add(t);
+					}
+					
+					state = State.START;
+				}
+				}
+			}
+			//number handled
+			
+			//string literal
+			case HAVE_STRING ->
+			{
+
+				ch = inp_eof[gpos];
+				
+				switch(ch)
+				{
+				
+				//escape sequence in string
+				case '\\' ->
+				{
+					char nextchar = inp_eof[gpos+1];
+					switch(nextchar)
+					{
+					
+					//acceptable escape sequences
+					case 'b','t','\\','n','f','r','"','\'' ->
+					{
+						gpos=gpos+2;
+						lexer_pos=lexer_pos+2;
+					}
+					
+					//error
+					default ->
+					{
+						Token t = new Token("Illegal character!", Kind.ERROR, token_pos, line, gpos, 2, dummy);
+						tokenset.add(t);
+						gpos++;
+						lexer_pos++;
+						state = State.START;
+					}
+					}
+				}
+				
+				//end string
+				case '\"' ->
+				{
+					lexer_pos++;
+					gpos++;
+					int string_len = lexer_pos - token_pos;
+					char[] strg = new char[string_len];
+					for(int i=0;i<string_len;i++)
+					{
+						strg[i]=inp_eof[gpos-string_len+i];
+					}
+					
+					//strg stored in stringval
+					String stringval = new String(strg);
+					Token t = new Token(stringval, Kind.STRING_LIT, token_pos, line, gpos-string_len, string_len, strg);
+					tokenset.add(t);
+					state = State.START;
+					System.out.println("hello");
+					System.out.println(strg);
+				}
+				
+				//default: string continues
+				default ->
+				{
+					lexer_pos++;
+					gpos++;
+				}
+				}
+			}
+			
+			
+			
 			}
 			
 		}
 		
 		
-		/*while(true) {
-			
-			//char ch = chinp[pos];
-			//switch(state)
-			
-		}*/
+
 	}
 
 	@Override
 	public IToken next() throws LexicalException {
-		// TODO Auto-generated method stub
 		
-		
-		return null;
+		Token tk =  tokenset.get(current_token);
+		current_token++;
+		if (tk.tkind == Kind.ERROR)
+		{
+			throw new LexicalException(tk.data, tk.line, tk.col);
+		}
+		return tk;
 	}
 
 	@Override
 	public IToken peek() throws LexicalException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return tokenset.get(current_token);
 	}
 
 }
--- edu/ufl/cise/plpfa22/Token.java
+++ edu/ufl/cise/plpfa22/Token.java
@@ -1,5 +1,8 @@
 package edu.ufl.cise.plpfa22;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Token implements IToken {
 
 	final Kind tkind; //token kind
@@ -8,8 +11,9 @@ public class Token implements IToken {
 	final int gpos; //global position
 	final int len; //length
 	final String data;
+	final char[] chardata;
 	
-	public Token(String data, Kind kind, int col, int line,  int gpos, int len) {
+	public Token(String data, Kind kind, int col, int line,  int gpos, int len, char[] chardata) {
 		// populate token's data
 		this.tkind = kind;
 		this.col = col;
@@ -17,6 +21,7 @@ public class Token implements IToken {
 		this.gpos = gpos;
 		this.len = len;
 		this.data = data;
+		this.chardata = chardata;
 	}
 
 	@Override
@@ -28,15 +33,20 @@ public class Token implements IToken {
 	@Override
 	public char[] getText() {
 		// TODO Auto-generated method stub
-		char[] text = this.data.toCharArray();
-		return text;
+		if(this.tkind==Kind.STRING_LIT)
+		{
+			return this.chardata;
+		}
+		else {
+			char[] text = this.data.toCharArray();
+		return text;}
 	}
 
 	@Override
 	public SourceLocation getSourceLocation() {
 		// TODO Auto-generated method stub
 		
-		SourceLocation sc = new SourceLocation(this.line, this.col);
+		SourceLocation sc = new SourceLocation(this.line, this.col+1);
 		return sc;
 	}
 
@@ -68,8 +78,118 @@ public class Token implements IToken {
 	@Override
 	public String getStringValue() {
 		// TODO Auto-generated method stub
+		char[] chdata = this.chardata;	
+		int len = chdata.length;
+		int newlen = len;
 		
-		return null;
+		List<Character> nparsed = new ArrayList<>();
+	
+		/*char[] parsed = new char[newlen];
+		int c=0;
+		for (int i=1; i<len-1;i++)
+		{
+			if (chdata[i]=='\\')
+			{
+					if(chdata[i+1]=='n')
+					{
+						parsed[c]='\n';
+					}
+					if(chdata[i+1]=='b')
+					{
+						parsed[c]='\b';
+					}
+					if(chdata[i+1]=='t')
+					{
+						parsed[c]='\t';
+					}
+					if(chdata[i+1]=='f')
+					{
+						parsed[c]='\f';
+					}
+					if(chdata[i+1]=='r')
+					{
+						parsed[c]='\r';
+					}
+					if(chdata[i+1]=='"')
+					{
+						parsed[c]='"';
+					}
+					if(chdata[i+1]=='\'')
+					{
+						parsed[c]='\'';
+					}
+					if(chdata[i+1]=='\\')
+					{
+						parsed[c]='\\';
+					}
+					c++;
+					i++;
+					
+			}
+
+			
+			else
+			{
+				parsed[c]=chdata[i];
+				c++;
+			}
+		}*/
+		
+		for (int i=1; i<len-1;i++)
+		{
+			if (chdata[i]=='\\')
+			{
+					if(chdata[i+1]=='n')
+					{
+						nparsed.add('\n');
+					}
+					if(chdata[i+1]=='b')
+					{
+						nparsed.add('\b');
+					}
+					if(chdata[i+1]=='t')
+					{
+						nparsed.add('\t');
+					}
+					if(chdata[i+1]=='f')
+					{
+						nparsed.add('\f');
+					}
+					if(chdata[i+1]=='r')
+					{
+						nparsed.add('\r');
+					}
+					if(chdata[i+1]=='"')
+					{
+						nparsed.add('"');
+					}
+					if(chdata[i+1]=='\'')
+					{
+						nparsed.add('\'');
+					}
+					if(chdata[i+1]=='\\')
+					{
+						nparsed.add('\\');
+					}
+					
+					i++;	
+			}
+			else
+			{
+				nparsed.add(chdata[i]);
+			}
+		}
+		
+		StringBuilder builder = new StringBuilder(nparsed.size());
+	    for(Character ch: nparsed)
+	    {
+	        builder.append(ch);
+	    }
+	    String stringval =  builder.toString();
+		
+		System.out.println(nparsed);
+		//String stringval = new String(nparsed);
+		return stringval;
 	}
 
 }
